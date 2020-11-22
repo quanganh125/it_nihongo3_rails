d59a645dd233ebdef72710fe16e7de20a84f81c1
Ubuntu <ubuntu@ip-172-31-7-56.ec2.internal>
2020-11-22 17:31:24 +0000

    Add image on book

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,9 @@ Rails.application.routes.draw do
   root 'static_pages#home'
   devise_for :users
   resources :books
+  
+  devise_scope :user do  
+   get '/users/sign_out' => 'devise/sessions#destroy'     
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
